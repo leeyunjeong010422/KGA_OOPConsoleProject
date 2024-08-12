62f2465028e2bab0f9a474fef9f899614f6bd0d2
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-08-12 13:02:56 +0900

    Add: SrartCode

---
+++ Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public class Game
+    {
+        private bool isRunning;
+
+        private Scene[] scenes;
+        private Scene curScene;
+
+        public Scene CurScene { get { return curScene; } }
+
+        private Player player;
+        public Player Player { get { return player; } set { player = value; } }
+
+
+    }
+}
