c02cc61e2a70cd7f61b34adfae78c8a03cdd99c8
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-08-16 11:37:59 +0900

    docs: 포켓몬별 hp 감소 기능 구현해야함

--- OOPConsoleProject/PoketMonster/PoketMonster.cs
+++ OOPConsoleProject/PoketMonster/PoketMonster.cs
@@ -1,5 +1,7 @@
 ﻿namespace OOPConsoleProject.PoketMonsters
 {
+    //선택한 포켓몬 별로 hp 감소하는 기능 구현 X
+    //현재 초기 설정한 30으로 모두 동일하게 감소
     public class PoketMonster
     {
         protected int attack = 30;
@@ -9,6 +11,38 @@
             set { attack = value; }
         }
 
+    }
+
+    public class JiuPoketMonster : PoketMonster
+    {
+        public JiuPoketMonster()
+        {
+            Attack = 40;
+        }
+    }
+
+    public class IseulPoketMonster : PoketMonster
+    {
+        public IseulPoketMonster()
+        {
+            Attack = 35;
+        }
+    }
+
+    public class UngPoketMonster : PoketMonster
+    {
+        public UngPoketMonster()
+        {
+            Attack = 50;
+        }
+    }
+
+    public class BomPoketMonster : PoketMonster
+    {
+        public BomPoketMonster()
+        {
+            Attack = 45;
+        }
     }
 
 
